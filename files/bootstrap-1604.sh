7fe2fe7defc0d0488809d55b966e2a783c6c3e4b
Joe Topjian <joe@topjian.net>
2019-04-08 21:38:27 -0600

    Updating 16.04

--- files/bootstrap-1604.sh
+++ files/bootstrap-1604.sh
@@ -4,14 +4,14 @@ cd /root
 
 apt-get update -qq
 apt-get install -y dpkg-dev
-apt-get install -y dpkg-dev linux-headers-4.4.0-62-generic
+apt-get install -y dpkg-dev linux-headers-4.4.0-142-generic
 
-apt-get source linux-image-4.4.0-62-generic
-apt-get build-dep -y linux-image-4.4.0-62-generic
+apt-get source linux-image-4.4.0-142-generic
+apt-get build-dep -y linux-image-4.4.0-142-generic
 
 pushd linux-4.4.0
 cp /root/files/.config.1604 .config
-cp /usr/src/linux-headers-4.4.0-62-generic/Module.symvers .
+cp /usr/src/linux-headers-4.4.0-142-generic/Module.symvers .
 cp /root/files/bnx2.c.1604 drivers/net/ethernet/broadcom/bnx2.c
 make scripts
 make modules_prepare
@@ -24,7 +24,7 @@ wget http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/
 mkdir staging
 pushd staging
 zcat ../initrd.gz | cpio -iv
-cp /root/linux-4.4.0/drivers/net/ethernet/broadcom/bnx2.ko lib/modules/4.4.0-62-generic/kernel/drivers/net/ethernet/broadcom/bnx2.ko
+cp /root/linux-4.4.0/drivers/net/ethernet/broadcom/bnx2.ko lib/modules/4.4.0-142-generic/kernel/drivers/net/ethernet/broadcom/bnx2.ko
 find . -print0 | cpio -0 -H newc -ov | gzip -c > ../initrd.gz
 popd
 popd
